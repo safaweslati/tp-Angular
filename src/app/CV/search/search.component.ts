c40d610d5795ed88821a8a5f05b64912d6542250
salmaghabri <salma.ghabri@insat.ucar.tn>
2023-11-26 16:42:03 +0100

    search bar works

---
+++ src/app/CV/search/search.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs';
+import { Cv } from '../Model/Cv';
+import { CvService } from '../services/cv.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+})
+export class SearchComponent implements OnInit {
+  selectedCv!: Cv;
+
+  form!: FormGroup;
+  cvs!: Observable<Cv[]>;
+  constructor(private cvService: CvService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      search: new FormControl(null, {
+        updateOn: 'change',
+      }),
+    });
+
+    this.cvs = this.form.get('search')!.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.cvService.searchCvs(term))
+    );
+  }
+
+  selectCv($event: any) {
+    this.selectedCv = $event;
+    const link = ['cv', this.selectedCv.id];
+    this.router.navigate(link);
+  }
+}
